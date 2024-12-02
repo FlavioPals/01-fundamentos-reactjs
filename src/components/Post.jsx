d979438bff2e21cc31177138a26eb99d0bee76c4
FlavioPals <flaviopals@hotmail.com>
2024-12-02 14:46:34 -0300

    Formulário de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,40 @@
-import styles from './Post.module.css'
+import styles from './Post.module.css';
 
 export function Post() {
-    return(
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}></div>
-                <img className={styles.avatar} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" />
-                <div className={styles.authorInfo}>
-                    <strong>Flavio Pals</strong>
-                    <span>Web Developer</span>
-                </div>
-                <time dateTime=""></time>
-            </header>
-        </article>
-    )
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+          <div className={styles.authorInfo}>
+            <strong>Flavio Pals</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉<a href="">jane.design/doctorcare</a></p>
+        <p><a href="">#novoprojeto</a></p>
+        <p><a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  )
 }
